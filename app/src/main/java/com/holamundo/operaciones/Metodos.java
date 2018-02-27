3541e5c922f588ce142a81425de46c7d1885193e
“Isaac” <ivasquez2@cuc.edu.co>
2018-02-26 20:24:16 -0500

    funcionalidad de suma sin validar terminada

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,11 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by Sorelyth on 26/02/2018.
+ */
+
+public class Metodos {
+    public static double sumar(double N1, double N2){
+        return N1+N2;
+    }
+}
