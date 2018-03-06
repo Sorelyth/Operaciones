66b13ca2e4bb79ac2bddc5718ff59d6505e0cebe
Sorelyth <sore.vp@gmail.com>
2018-03-05 20:25:11 -0500

    Operaciones sin testing

--- app/src/main/java/com/holamundo/operaciones/Metodos.java
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -8,4 +8,12 @@ public class Metodos {
     public static double sumar(double N1, double N2){
         return N1+N2;
     }
+    public static double restar(double N1, double N2){
+        return N1-N2;
+    }
+    public static double multiplicar(double N1, double N2){return N1*N2;}
+    public static double dividir(double N1, double N2){
+        return N1/N2;
+    }
+
 }
